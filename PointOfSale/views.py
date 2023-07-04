57d514698feb1986d984616a74cfc218492bf463
Markjonel <rellesmarkjonel14@gmail.com>
2023-07-04 18:28:20 +0800

    pos

--- PointOfSale/views.py
+++ PointOfSale/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from ProductManagement.models import Product_crud
 from django.contrib.auth.decorators import login_required
-from .forms import CartQuantity_Form
+from .forms import *
 from .models import *
-from django.db.models import  Q
+from django.db.models import Q,Sum
 from django.http import JsonResponse
+from django.contrib import messages
 # Create your views here.
 
 
@@ -21,18 +22,56 @@ def search(request):
 
 @login_required()
 def pos(request):
-    pos_prod=Product_crud.objects.all().order_by('Category')
-    
-    if 'search_results' in request.session:
-        search_item = request.session['search_results']
-        
-        if search_item is not None:
-            pos_prod = Product_crud.objects.filter(Q(ItemName__icontains=search_item) | Q(Category__icontains=search_item))
-        else:
-            pos_prod = Product_crud.objects.all().order_by('Category')
-            request.session['search_results']=None
-    else:
-        pos_prod = Product_crud.objects.all().order_by('Category')
-    
-    return render(request, 'POS/pos.html',{'pos_prod':pos_prod})
-     
+    pos_prod = Product_crud.objects.all().order_by('Category')
+    cart = CartQuantity.objects.all()
+    form = CartQuantity_Form()
+    if request.method =='POST':
+        form = CartQuantity_Form(request.POST or None)
+        if form.is_valid():
+            prid = form.cleaned_data['product_id']
+            form.save()
+            prod  = Product_crud.objects.get(id=prid)
+            
+            price  = prod.Price
+            quantity = int(form.cleaned_data["quantity"])
+            total_price = price * quantity
+            cartitem  = CartQuantity.objects.filter(product_id=prid)
+            
+            if cartitem.count() > 1: 
+                print('you selected many')
+                min = cartitem[0].id
+                
+                
+                for carts in cartitem:
+                    if carts.id < min:
+                        min = carts.id
+                
+                base_cart = CartQuantity.objects.get(id=min)       
+                if base_cart.quantity + quantity <=Product_crud.objects.get(id=base_cart.product_id).Current_Stock:
+                    base_cart.quantity += quantity 
+                    base_cart.total_price += total_price
+                    base_cart.total_price += total_price
+                    base_cart.save()
+                else:
+                    messages.error (request,'The desired quantity exceeds current Stock')
+                for carts in cartitem:
+                  if not carts.id == min:
+                      carts.delete() 
+            else:
+                cartitem = CartQuantity.objects.get(product_id=prid)       
+                total_price = quantity * price   
+                itemname = prod.ItemName
+                itemsize = prod.Sizes
+                
+                cartitem.total_price = total_price
+                cartitem.name = itemname
+                cartitem.size = itemsize
+                cartitem.save()
+            form = CartQuantity_Form()
+                      
+    context = {'pos_prod':pos_prod,'carts':cart,'form':form}
+    return render(request,'POS/pos.html',context)    
+            
+       
+
+   
